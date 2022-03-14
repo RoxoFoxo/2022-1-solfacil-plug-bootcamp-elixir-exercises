5edf783aa6d61b2744fe24d2ab75e4b9e920c459
RoxoFoxo <gab.apfprado@gmail.com>
2022-03-14 19:12:01 -0300

    Updated legibility on Bird Count and Pacman Rules

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,20 +1,13 @@
 defmodule BirdCount do
   def today([today | _]), do: today
-
   def today(_), do: nil
 
-  def increment_day_count([today | tail]) do
-    [today + 1 | tail]
-  end
-
+  def increment_day_count([today | tail]), do: [today + 1 | tail]
   def increment_day_count(_), do: [1]
 
-  def has_day_without_birds?(list) do
-    Enum.member?(list, 0)
-  end
+  def has_day_without_birds?(list), do: Enum.member?(list, 0)
 
   def total([]), do: 0
-
   def total(list), do: Enum.sum(list)
 
   def busy_days(list) do
--- pacman-rules/lib/rules.ex
+++ pacman-rules/lib/rules.ex
@@ -1,45 +1,17 @@
 defmodule Rules do
   def eat_ghost?(power_pellet_active, touching_ghost) do
-    # Please implement the eat_ghost?/2 function
-    case power_pellet_active and touching_ghost do
-      true ->
-        true
-
-      false ->
-        false
-    end
+    power_pellet_active and touching_ghost
   end
 
   def score?(touching_power_pellet, touching_dot) do
-    # Please implement the score?/2 function
-    case touching_power_pellet or touching_dot do
-      true ->
-        true
-
-      false ->
-        false
-    end
+    touching_power_pellet or touching_dot
   end
 
   def lose?(power_pellet_active, touching_ghost) do
-    # Please implement the lose?/2 function
-    case power_pellet_active do
-      false when touching_ghost ->
-        true
-
-      _ ->
-        false
-    end
+    not power_pellet_active and touching_ghost
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-    # Please implement the win?/3 function
-    case lose?(power_pellet_active, touching_ghost) do
-      false when has_eaten_all_dots ->
-        true
-
-      _ ->
-        false
-    end
+    has_eaten_all_dots and not lose?(power_pellet_active, touching_ghost)
   end
 end
